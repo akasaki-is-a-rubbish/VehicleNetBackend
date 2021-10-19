e28747db906fd3a26a1ef094dc728f48c2929c8a
lideming <me@yuuza.net>
2021-10-19 23:24:21 +0800

    add ApiController

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleNetBackend
+{
+    [ApiController]
+    [Route("/api")]
+    public class ApiController : Controller
+    {
+        [HttpGet("ping")]
+        public IActionResult GetPing()
+        {
+            return new JsonResult(new {
+                status = "ok"
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -54,6 +54,7 @@ namespace VehicleNetBackend
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
